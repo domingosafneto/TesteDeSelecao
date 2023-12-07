c51ff0d98b6dee259d71c60ece3f52d8808c6367
Domingos Neto <domingosafneto@gmail.com>
2023-12-07 17:47:15 -0300

    commit inicial

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria {
+        public int Numero { get; }
+
+        public string Titular { get; set; }
+
+        public double DepositoInicial { get; set; }
+
+        public double Saldo { get; set; }
+
+
+        public ContaBancaria(int pNumero, string pTitular, double pDepositoInicial)
+        {
+            Numero = pNumero;
+            Titular = pTitular;
+            DepositoInicial = pDepositoInicial;
+        }
+
+        public ContaBancaria(int pNumero, string pTitular)
+        {
+            Numero = pNumero;
+            Titular = pTitular;
+        }
+
+        public void Deposito(double pQuantia)
+        {
+            Saldo = pQuantia + Saldo;
+        }
+
+        public void Saque(double pQuantia)
+        {
+            Saldo = Saldo - pQuantia;
+        }
+
+
+    }
+}
